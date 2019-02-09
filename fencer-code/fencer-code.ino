da13687bae00ad30801428637caad4ffecf084b5
Vladimir <vlada.krsmanovic@gmail.com>
2019-02-09 17:17:49 +0100

    added files for 3D printing

--- fencer-code/fencer-code.ino
+++ fencer-code/fencer-code.ino
@@ -13,7 +13,7 @@ void setup() {
   //configure pin 2 as an input and enable the internal pull-up resistor
   pinMode(2, INPUT_PULLUP);
   pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
+  //pinMode(8, OUTPUT);
   pinMode(buzzer, OUTPUT);
 }
 
@@ -31,15 +31,15 @@ void loop() {
   }
   if(millis() - last_hit < TIME_BETWEEN)
   {
-    light = HIGH;
-    tone(buzzer, 1500);
+    //light = HIGH;
+    tone(buzzer, 1000);
   }
   else
   {
     light = LOW;
     noTone(buzzer);
   }
-  digitalWrite(8, light);
+  //digitalWrite(8, light);
   // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
   // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
   // button's pressed, and off when it's not:
